054a99d6f8e425c80f5db614ac089cb78b1cf338
student <student@ip-172-31-52-234.us-west-2.compute.internal>
2021-03-23 16:55:42 -0700

    Began the first level!

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,43 @@
 package _01_houses;
 
+import java.util.Random;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
+	Robot rob = new Robot();
+
 	public void run() {
-		
+		rob.setX(50);
+		rob.setY(530);
+		Random rand = new Random();
+	for(int homes = 0; homes < 9; homes++) {
+	int randomHeight = rand.nextInt(2) + 1;
+	drawHouse(randomHeight);
+	}
+	}
+	
+	void drawHouse(String height) {
+		int heightInt = 0;
+		if(height.equalsIgnoreCase("small")) {
+			heightInt = 60;
+		} else if(height.equalsIgnoreCase("medium")) {
+			heightInt = 120;
+		} else if(height.equalsIgnoreCase("large")) {
+			heightInt = 250;
+		}
+		rob.penDown();
+		rob.setPenColor(255, 5, 5);
+		rob.setPenWidth(10);
+		rob.setSpeed(200);
+		rob.move(heightInt);
+		rob.turn(90);
+		rob.move(30);
+		rob.turn(90);
+		rob.move(heightInt);
+		rob.turn(270);
+		rob.setPenColor(5, 255, 5);
+		rob.move(60);
+		rob.turn(270);
 	}
 }
