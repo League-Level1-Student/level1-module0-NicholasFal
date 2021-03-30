ad87c69b80072eb88b1c839c77cd5fc6810088e8
student <student@ip-172-31-49-221.us-west-2.compute.internal>
2021-03-30 16:48:28 -0700

    I learned about objects!

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -8,16 +8,31 @@ public class Houses {
 	Robot rob = new Robot();
 
 	public void run() {
+		rob.setWindowColor(0, 0, 0);
 		rob.setX(50);
 		rob.setY(530);
+		rob.setPenWidth(10);
+		rob.setSpeed(200);
 		Random rand = new Random();
-	for(int homes = 0; homes < 9; homes++) {
-	int randomHeight = rand.nextInt(2) + 1;
-	drawHouse(randomHeight);
+		for (int homes = 0; homes < 9; homes++) {
+			int randomHeight = rand.nextInt(3);
+			String size = "unnamed";
+			String colors = "unnamed";
+			if (randomHeight == 0) {
+				size = "small";
+				colors = "orange";
+			} else if (randomHeight == 1) {
+				size = "medium";
+				colors = "green";
+			} else {
+				size = "large";
+				colors = "purple";
+			}
+			drawHouse(size, colors);
+		}
 	}
-	}
-	
-	void drawHouse(String height) {
+
+	void drawHouse(String height, String color) {
 		int heightInt = 0;
 		if(height.equalsIgnoreCase("small")) {
 			heightInt = 60;
@@ -27,17 +42,37 @@ public class Houses {
 			heightInt = 250;
 		}
 		rob.penDown();
-		rob.setPenColor(255, 5, 5);
-		rob.setPenWidth(10);
-		rob.setSpeed(200);
+		if(color.equals("orange")) {
+			rob.setPenColor(255, 105, 1);
+		} else if(color.equals("green")) {
+			rob.setPenColor(25, 135, 35);
+		} else {
+			rob.setPenColor(127, 1, 127);
+		}
 		rob.move(heightInt);
-		rob.turn(90);
-		rob.move(30);
-		rob.turn(90);
+		if(height.equalsIgnoreCase("large")) {
+			drawFlatRoof();
+		} else {
+			drawPeakedRoof();
+		}
 		rob.move(heightInt);
 		rob.turn(270);
 		rob.setPenColor(5, 255, 5);
 		rob.move(60);
 		rob.turn(270);
+	}
+
+	void drawPeakedRoof() {
+		rob.turn(45);
+		rob.move(25);
+		rob.turn(90);
+		rob.move(25);
+		rob.turn(45);
+	}
+
+	void drawFlatRoof() {
+		rob.turn(90);
+		rob.move(25);
+		rob.turn(90);
 	}
 }
